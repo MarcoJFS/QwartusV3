47e3dae8ed60e5652bd2a46c43b4fbaf3a0d8cd8
MarcoJFS <marcovarandinhas@hotmail.com>
2016-11-20 11:28:34 +0000

    Menu de procura quando está logado
    O menu de procura está feito quando está logado...

---
+++ Qwartus/app/src/main/java/pt/ipleira/estg/qwartus/MenuLoginFeito.java
@@ -0,0 +1,13 @@
+package pt.ipleira.estg.qwartus;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MenuLoginFeito extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_login_feito);
+    }
+}
