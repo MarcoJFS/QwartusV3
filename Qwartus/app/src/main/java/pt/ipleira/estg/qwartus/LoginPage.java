ce7ccaa09094ac90c6f64627217cda49745a6999
MarcoJFS <marcovarandinhas@hotmail.com>
2016-11-22 17:17:51 +0000

    Floating Button
    Floating Button na página Login(para testes)

--- Qwartus/app/src/main/java/pt/ipleira/estg/qwartus/LoginPage.java
+++ Qwartus/app/src/main/java/pt/ipleira/estg/qwartus/LoginPage.java
@@ -1,13 +1,28 @@
 package pt.ipleira.estg.qwartus;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
 
 public class LoginPage extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
+
+        FloatingActionButton FloatingButtonPlus = (FloatingActionButton) findViewById(R.id.FloatingButtonPlus);
+        FloatingButtonPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent("android.intent.qwartus.REGISTERPAGE");
+                startActivity(i);
+            }
+        });
+
     }
 }
--- Qwartus/app/src/main/java/pt/ipleira/estg/qwartus/ac_Qwartus.java
+++ Qwartus/app/src/main/java/pt/ipleira/estg/qwartus/ac_Qwartus.java
@@ -1,16 +1,14 @@
 package pt.ipleira.estg.qwartus;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
-import android.view.View.OnClickListener;
-
-import static pt.ipleira.estg.qwartus.R.id.button;
 
 public class ac_Qwartus extends AppCompatActivity {
 
